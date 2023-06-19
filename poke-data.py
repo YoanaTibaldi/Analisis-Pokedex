7ca3308773576fff7892e54d9309aa2bebaf73dd
YoanaTibaldi <71615448+YoanaTibaldi@users.noreply.github.com>
2023-06-18 21:37:19 -0300

    Recoleccion de Puntos Base

--- poke-data.py
+++ poke-data.py
@@ -10,16 +10,28 @@ api = "https://api.pokemon.com/el/api/pokedex"
 Tomar las urls de la api y pasarla como parametro a la url original. Con BeautifulSoup extraigo los datos que quiero de la página
 """
 
+
 def build_url(): 
     response = requests.get(api)
     data = json.loads(response.text) 
 
     for detail in data:
-        detailURL = detail["detailPageURL"]
-        urlUpdate = f'https://api.pokemon.com{detailURL}'
-        url_response = requests.get(urlUpdate)
-        soup = BeautifulSoup(url_response.text)
-        print(soup)
+        detail_url = detail["detailPageURL"]
+        url_update = f'https://api.pokemon.com{detail_url}'
+        url_response = requests.get(url_update)
+        soup = BeautifulSoup(url_response.text, 'html.parser')
+        ul_element = soup.find_all("ul", class_="gauge")
+        div_poke_active = soup.find("div", class_="pokemon-stats-info active")
+        span_text = div_poke_active.find_all('span')
 
+        for ul in ul_element:
+            data_points = ul.find('li', attrs={'data-value': True})
+            image = soup.find('img', class_='active')
+            print('values: ', data_points['data-value'])                
+        for span in span_text:
+            print(span.get_text())
+                    # for character in content:
+                
 
 build_url()
+
